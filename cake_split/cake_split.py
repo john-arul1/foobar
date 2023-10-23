b453d906d8f006a7608f7e4d1e4e6211a40ecb1a
john-arul1 <34236264+john-arul1@users.noreply.github.com>
2023-10-23 15:13:53 +0530

    Create cake_split.py

---
+++ cake_split/cake_split.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 23:10:45 2023
+
+@author: John Arul
+
+google challenge split cake into equal parts.
+
+Extract number of full identical substrings
+
+The solution is based on the symmetry of the given string. 
+
+"""
+
+
+def shift_compare(s,n):
+    # rotate and compare
+    N=len(s)
+    
+    equal=True
+    for i in range(len(s)):
+        if s[i]!= s[(i+n)%N]:    
+           equal =False
+           break
+    return equal     
+    
+
+def solution(s):
+    
+    for i in range(1,len(s)):
+         
+        #if i devides len(s)
+        N=len(s)
+        if (abs(N/i-N//i) ==0):
+            if shift_compare(s,i):
+               return N//i 
+            
+    return 1
+        
+        
+# Some tests          
+#s="abcdabcdabcd"
+s="abccbaabccba"
+#s='abcabcabcabcabc'
+#s='abcdefghijkl'
+#s=random.choices("abc", k=7)
+# z=''
+# for i in range(10):
+#       z+=s
+z=s    
+print(len(z))    
+d=solution(z)
+print (d)        
